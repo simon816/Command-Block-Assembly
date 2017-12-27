f8e3170b9808f28b210f0ae2beaf59668948f6e8
chorman0773 <chorman64@gmail.com>
2017-12-27 17:28:07 -0500

    Merge PR #2, add stddef, stdbool and stdarg
    * Create stddef.h
    
    Add NULL def
    
    * Create stdbool.h
    
    * Create stdarg.h
    
    Added basic variadic useage containers and functions.
    Only use va_start, va_arg, and va_end
    Requires Co-operation with compiler and complete implementation.

---
+++ std/stdbool.h
@@ -0,0 +1,11 @@
+#ifndef mclib_stdbool_h__17_12_08____09_01_29
+#define mclib_stdbool_h__17_12_08____09_01_29
+#include <mclib.h>
+#include <stddef.h>
+
+#define bool int
+#define true 1
+#define false 0
+
+
+#endif
---
+++ std/stddef.h
@@ -0,0 +1,7 @@
+#ifndef mclib_stddef_h__17_12_11____09_13_47
+#define mclib_stddef_h__17_12_11____09_13_47
+#include <mclib.h>
+
+#define NULL 0
+
+#endif
